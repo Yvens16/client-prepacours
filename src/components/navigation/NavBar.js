7b64a3c310bf6520963b1770c8e84aa7c4290866
Yvens 16 <yvensbelaston@gmail.com>
2019-01-30 00:17:49 +0100

    Fixed navbar (css)

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -12,6 +12,7 @@ const NavBar = () => {
         <li>Logo middle</li>
         <li className="fb-share-button" data-href="https://www.facebook.com/fabuno/" data-layout="button_count" data-size="small" data-mobile-iframe="true">
         <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Ffabuno%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">
+        Partager
         </a>
         </li>
         <li><button id='reservation'> <Link to='/application'>Réserver un cours</Link></button></li>
