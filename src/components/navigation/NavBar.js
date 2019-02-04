f0688d1f29d6bbff8e294c9a7e9859e32e03b110
Yvens 16 <yvensbelaston@gmail.com>
2019-02-05 00:06:11 +0100

    restarted Navbar + install react16.7 + first use of React Fragment

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -4,21 +4,31 @@ import './_navbar.scss';
 
 const NavBar = () => {
   return (
-    <main className='navigation'>
-    <nav>
-      <ul>
-        <li>Logo</li>
-        <li> <a href="tel:+33627269473"> 06 27 26 94 73</a></li>
-        <li>Logo middle</li>
-        <li className="fb-share-button" data-href="https://www.facebook.com/fabuno/" data-layout="button_count" data-size="small" data-mobile-iframe="true">
-        <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Ffabuno%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">
-        Partager
-        </a>
-        </li>
-        <li><button id='reservation'> <Link to='/application'>Réserver un cours</Link></button></li>
-      </ul>
-    </nav>
-    </main>
+    <>
+    <header className="main_h">
+      <div className="row">
+        <Link to='/' className="logo">Logo</Link>
+
+        <div className="mobile-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="tel:+33627269473">06 27 26 94 73</a></li>
+            <div className="fb-share-button" data-href="https://www.facebook.com/fabuno/" data-layout="button_count" data-size="small" data-mobile-iframe="true">
+            <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Ffabuno%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">
+            Partager
+            </a>
+            </div>
+            <li><Link className='a' to=''>Partager</Link></li>
+            <li><Link className='a' to='/application'>Réserver un cours</Link></li>
+          </ul>
+        </nav>
+      </div>  
+    </header>
+    </>
   )
 } 
 
