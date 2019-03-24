fed8e28e6cb62f21dea10e7ffc70a29d3b6eb7e1
Yvens 16 <yvensbelaston@gmail.com>
2019-03-24 18:44:04 +0100

    main.css uncommented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import './main.css';
+import './main.css';
 import App from './App';
 // import Test from './Test';
 // import Form from '../src/components/standalone/form/Form';
